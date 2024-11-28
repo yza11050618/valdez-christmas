e20a0e6530068a043813a49e640a3efa886fc373
yza11050618 <valdezyza4@gmail.com>
2024-11-28 14:54:58 -0100

    first commit

---
+++ toggle_menu.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerNav = document.querySelector(".header__nav");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerNav.classList.toggle("open");
+});
